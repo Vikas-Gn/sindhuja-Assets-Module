04d8196b6895ae2eda578e3c544d5539ee9bbacd
Vikas-Gn <vikasgn2000@gmail.com>
2025-06-19 18:57:35 +0530

    updated-asset

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 8020;
+const port = 3030;
 
 // Middleware
 app.use(cors());
